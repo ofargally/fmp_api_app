e5c329410dddb09e03ad0304ff014d622c11d69f
ofargally <ofargally@colgate.edu>
2025-01-11 05:26:59 -0500

    fixed minor errors

--- back-end/utils.py
+++ back-end/utils.py
@@ -3,8 +3,8 @@ from typing import List, Dict, Any
 
 
 async def fetch_financial_data(api_key: str, company: str = "AAPL") -> List[Dict[str, Any]]:
-    api_url = f"https://financialmodelingprep.com/api/v3/income-statement/{
-        company}?period=annual&apikey={api_key}"
+    url = "https://financialmodelingprep.com/api/v3/income-statement"
+    api_url = f"{url}/{company}?period=annual&apikey={api_key}"
     try:
         response = requests.get(api_url)
         response.raise_for_status()
